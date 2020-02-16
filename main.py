51cc1504553f6001f5269566aa82e50a57ce5455
inar1 <inar1@protonmail.com>
2020-02-16 21:21:40 +0200

    update main.py

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ import os
 
 SEARCH_API = 'https://api.github.com/search/repositories'
 REPOS_PATH = './data'
+DURATION = 90
+STARS = 100
 
 
 def main():
     today = datetime.date.today()
-    past = today - datetime.timedelta(days=30)
+    past = today - datetime.timedelta(days=DURATION)
 
     api_params = '?q=web'
     api_params += '+language:{}'.format('php')
-    api_params += '+stars:>{}'.format(100)
+    api_params += '+stars:>{}'.format(STARS)
     api_params += '+pushed:{}'.format(past)
 
     res = requests.get(SEARCH_API + api_params)
@@ -26,17 +28,15 @@ def main():
         full_name = repo['full_name']
         repos[full_name] = repo['clone_url']
 
-    print(glob.glob('./*'))
     if REPOS_PATH not in glob.glob('./*'):
         print('{} does not exist, running mkdir'.format(REPOS_PATH))
         os.system('mkdir {}'.format(REPOS_PATH))
 
-    dir_list = glob.glob(REPOS_PATH + '/*')
-    print(dir_list)
+    repos_list = glob.glob(REPOS_PATH + '/*')
 
     for key in repos.keys():
         path = REPOS_PATH + '/' + key.replace('/', '_')
-        if path not in dir_list:
+        if path not in repos_list:
             print('Cloning "{}"...'.format(key))
             clone = 'git clone ' + repos[key] + ' {}'.format(path)
             print(clone)
